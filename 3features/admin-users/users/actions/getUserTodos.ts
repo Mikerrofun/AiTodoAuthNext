fcfbfd88a93ad6d0967c16c347b636983044399d
Mikerrofun <mitja3375@gmail.com>
2026-06-18 01:38:49 +0300

    ServerAction for get user todo(without connectiong to admin panel)

--- 3features/admin-users/users/actions/getUserTodos.ts
+++ 3features/admin-users/users/actions/getUserTodos.ts
@@ -4,37 +4,25 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/5shared/lib/auth/authOptions";
 import { prisma } from "@/prisma/client";
 import { ActionResult } from "@/5shared/lib/types/action-result";
+import { TodoItem } from "@entities/todo";
 
-export type UserItem = {
-  id: number;
-  login: string;
-  role: string;
-  createdAt: Date;
-};
-
-export async function getUsers(): Promise<ActionResult<UserItem[]>> {
+export async function getUserTodos(userId: number): Promise<ActionResult<TodoItem[]>> {
   const session = await getServerSession(authOptions);
 
-  
   if (!session?.user) {
     return { status: "error", message: "Не авторизован" };
   }
 
-  if (session.user?.role !== "ADMIN") {
+  if (session.user.role !== "ADMIN") {
     return { status: "error", message: "Недостаточно прав" };
   }
 
   try {
-    const users = await prisma.user.findMany({
-      select: {
-        id: true,
-        login: true,
-        role: true,
-        createdAt: true,
-      },
+    const todos = await prisma.todo.findMany({
+      where: { userId },
     });
 
-    return { status: "success", data: users };
+    return { status: "success", data: todos };
   } catch {
     return { status: "error", message: "Что-то пошло не так" };
   }
